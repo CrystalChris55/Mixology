b3db984ce37c154c552a3689402202df7e56d87b
CrystalChris55 <lacasse249@gmail.com>
2024-12-29 16:17:36 -0500

    Big update, file restructuring, item icons finalized, containers finalized, blah blah blah.

---
+++ Contents/mods/Mixology/42/media/lua/server/Items/Mixology_MiscDistributions.lua
@@ -0,0 +1,237 @@
+---------------------------------------------------
+-- Glassware Distributions And Sandbox Spawn File          --
+---------------------------------------------------
+
+
+-- Sandbox Settings Placeholders -- TO BE ADDED
+--local FluidContainerSpawnChance = SandboxVars.Mixology.FluidContainerSpawnChance
+--local AlcoholContainerSpawnChance = SandboxVars.Mixology.AlcoholContainerSpawnChance
+--local GlasswareSpawnChance = SandboxVars.Mixology.GlasswareSpawnChance
+
+-- Theoretical example of future sandbox options.
+-- table.insert(ProceduralDistributions["list"]["BarCounterLiquor"].items, AlcoholContainerSpawnChance * 0.11);
+
+------------------------ Distros --------------------------------------
+
+---------------------------
+---Ice Cubes and Icebag----
+---------------------------
+
+--Hot Dog Stand
+table.insert(ProceduralDistributions["list"]["HotdogStandDrinks"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["HotdogStandDrinks"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["HotdogStandDrinks"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["HotdogStandDrinks"].items,  20.20);
+
+-- FreezerGeneric
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items,  20.20);
+
+-- FreezerRich
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items,  20.20);
+
+-- FreezerTrailerPark
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items,  20.20);
+
+-- FreezerFarmStorage
+table.insert(ProceduralDistributions["list"]["FreezerFarmStorage"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerFarmStorage"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerFarmStorage"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerFarmStorage"].items,  20.20);
+
+-- FreezerGarage
+table.insert(ProceduralDistributions["list"]["FreezerGarage"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerGarage"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerGarage"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerGarage"].items,  20.20);
+
+-- FreezerIceCream
+table.insert(ProceduralDistributions["list"]["FreezerIceCream"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerIceCream"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerIceCream"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerIceCream"].items,  20.20);
+
+-- FreezerDrugLab
+table.insert(ProceduralDistributions["list"]["FreezerDrugLab"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerDrugLab"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerDrugLab"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerDrugLab"].items,  20.20);
+
+-- FreezerFrozenFood
+table.insert(ProceduralDistributions["list"]["FreezerFrozenFood"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["FreezerFrozenFood"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["FreezerFrozenFood"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["FreezerFrozenFood"].items,  20.20);
+
+-- FreezerWater
+-- table.insert(ProceduralDistributions["list"]["FreezerWater"].items, "Mixology.Icecube");
+-- table.insert(ProceduralDistributions["list"]["FreezerWater"].items,  20.20);
+-- table.insert(ProceduralDistributions["list"]["FreezerWater"].items, "Mixology.Icebag");
+-- table.insert(ProceduralDistributions["list"]["FreezerWater"].items,  20.20);
+
+-- FreezerVIPLounge
+--table.insert(ProceduralDistributions["list"]["FreezerVIPLounge"].items, "Mixology.Icecube");
+--table.insert(ProceduralDistributions["list"]["FreezerVIPLounge"].items,  20.20);
+--table.insert(ProceduralDistributions["list"]["FreezerVIPLounge"].items, "Mixology.Icebag");
+--table.insert(ProceduralDistributions["list"]["FreezerVIPLounge"].items,  20.20);
+
+-- SafehouseFreezer
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer"].items,  20.20);
+
+-- SafehouseFreezer_Late
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Late"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Late"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Late"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Late"].items,  20.20);
+
+-- SafehouseFreezer_Mid
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Mid"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Mid"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Mid"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["SafehouseFreezer_Mid"].items,  20.20);
+
+-- KitchenFreezer
+--table.insert(ProceduralDistributions["list"]["KitchenFreezer"].items, "Mixology.Icecube");
+--table.insert(ProceduralDistributions["list"]["KitchenFreezer"].items,  20.20);
+--table.insert(ProceduralDistributions["list"]["KitchenFreezer"].items, "Mixology.Icebag");
+--table.insert(ProceduralDistributions["list"]["KitchenFreezer"].items,  20.20);
+
+-- RestaurantKitchenFreezer
+table.insert(ProceduralDistributions["list"]["RestaurantKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["RestaurantKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["RestaurantKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["RestaurantKitchenFreezer"].items,  20.20);
+
+-- SpiffosKitchenFreezer
+table.insert(ProceduralDistributions["list"]["SpiffosKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["SpiffosKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["SpiffosKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["SpiffosKitchenFreezer"].items,  20.20);
+
+-- DinerKitchenFreezer
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items,  20.20);
+
+-- BakeryKitchenFreezer
+table.insert(ProceduralDistributions["list"]["BakeryKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["BakeryKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["BakeryKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["BakeryKitchenFreezer"].items,  20.20);
+
+-- ChineseKitchenFreezer
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items,  20.20);
+
+-- ItalianKitchenFreezer
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items,  20.20);
+
+-- MexicanKitchenFreezer
+table.insert(ProceduralDistributions["list"]["MexicanKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["MexicanKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["MexicanKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["MexicanKitchenFreezer"].items,  20.20);
+
+-- SeafoodKitchenFreezer
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items,  20.20);
+
+-- SushiKitchenFreezer
+table.insert(ProceduralDistributions["list"]["SushiKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["SushiKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["SushiKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["SushiKitchenFreezer"].items,  20.20);
+
+-- WesternKitchenFreezer
+table.insert(ProceduralDistributions["list"]["WesternKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["WesternKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["WesternKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["WesternKitchenFreezer"].items,  20.20);
+
+-- DeepFryKitchenFreezer
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items,  20.20);
+
+-- BurgerKitchenFreezer
+table.insert(ProceduralDistributions["list"]["BurgerKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["BurgerKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["BurgerKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["BurgerKitchenFreezer"].items,  20.20);
+
+-- PizzaKitchenFreezer
+table.insert(ProceduralDistributions["list"]["PizzaKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["PizzaKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["PizzaKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["PizzaKitchenFreezer"].items,  20.20);
+
+-- CrepeKitchenFreezer
+--table.insert(ProceduralDistributions["list"]["CrepeKitchenFreezer"].items, "Mixology.Icecube");
+--table.insert(ProceduralDistributions["list"]["CrepeKitchenFreezer"].items,  20.20);
+--table.insert(ProceduralDistributions["list"]["CrepeKitchenFreezer"].items, "Mixology.Icebag");
+--table.insert(ProceduralDistributions["list"]["CrepeKitchenFreezer"].items,  20.20);
+
+-- JaysKitchenFreezer
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items,  20.20);
+
+-- TheatreKitchenFreezer
+table.insert(ProceduralDistributions["list"]["TheatreKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["TheatreKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["TheatreKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["TheatreKitchenFreezer"].items,  20.20);
+
+-- ArenaKitchenFreezer
+table.insert(ProceduralDistributions["list"]["ArenaKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["ArenaKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["ArenaKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["ArenaKitchenFreezer"].items,  20.20);
+
+-- CatfishKitchenFreezer
+table.insert(ProceduralDistributions["list"]["CatfishKitchenFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["CatfishKitchenFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["CatfishKitchenFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["CatfishKitchenFreezer"].items,  20.20);
+
+-- ButcherFreezer
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items, "Mixology.Icecube");
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items,  20.20);
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items, "Mixology.Icebag");
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items,  20.20);
+
+--------------------------------------------
+--  Misc Items Spawns umbrellas/mints/etc --
+--------------------------------------------
+
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "Mixology.MintPackage");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items,  15.20);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "Mixology.SkewerPack");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items,  15.20);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "Mixology.StrawPackage");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items,  15.20);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "Mixology.UmbrellaPackage");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items,  15.20);
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items, "Mixology.CherryJar");
+table.insert(ProceduralDistributions["list"]["BarCounterMisc"].items,  15.20);
